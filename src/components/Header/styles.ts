1c66dfabfc755b9b1c1770cb51e69c149693f7ba
Kaique Bosco <kaique.bosco@hotmail.com>
2023-09-11 16:53:14 -0300

    applying redux

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,5 @@
-import styled from 'styled-components'
-import { cores } from '../../styles'
+import styled from "styled-components"
+import { cores } from "../../styles"
 
 export const Header = styled.header`
   background-image: linear-gradient(
